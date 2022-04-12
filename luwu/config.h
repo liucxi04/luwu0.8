09ec9a2a970fb6123d9131ef7a8f9aacb32e371f
liucxi <liucxi@foxmail.com>
2022-04-12 20:46:00 +0800

    config module coding and test finish

--- luwu/config.h
+++ luwu/config.h
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <utility>
 #include <vector>
+#include <functional>
 #include <map>
 #include <set>
 #include <list>
@@ -41,11 +42,16 @@ namespace liucxi {
 
         virtual bool fromString(const std::string &val) = 0;
 
+        virtual std::string getTypeName() const = 0;
+
     protected:
         std::string m_name;         /// 配置名称
         std::string m_describe;     /// 配置描述
     };
 
+    /**
+     * @brief 类型转换
+     * */
     template<typename From, typename To>
     class LexicalCast {
     public:
@@ -54,14 +60,17 @@ namespace liucxi {
         }
     };
 
+    /**
+     * 模板偏特化，string to vector<T>，下同
+     * */
     template<typename To>
     class LexicalCast<std::string, std::vector<To>> {
     public:
-        std::vector<To> operator() (const std::string &v) {
+        std::vector<To> operator()(const std::string &v) {
             YAML::Node node = YAML::Load(v);
             typename std::vector<To> vec;
             std::stringstream ss;
-            for (auto i : node) {
+            for (auto i: node) {
                 ss.str("");
                 ss << i;
                 vec.push_back(LexicalCast<std::string, To>()(ss.str()));
@@ -73,10 +82,10 @@ namespace liucxi {
     template<typename From>
     class LexicalCast<std::vector<From>, std::string> {
     public:
-        std::string operator() (const std::vector<From> &v) {
+        std::string operator()(const std::vector<From> &v) {
             YAML::Node node;
             std::stringstream ss;
-            for (auto &i : v) {
+            for (auto &i: v) {
                 node.push_back(YAML::Load(LexicalCast<From, std::string>()(i)));
             }
             ss << node;
@@ -87,14 +96,14 @@ namespace liucxi {
     template<typename To>
     class LexicalCast<std::string, std::set<To>> {
     public:
-        std::set<To> operator() (const std::string &v) {
+        std::set<To> operator()(const std::string &v) {
             YAML::Node node = YAML::Load(v);
             typename std::set<To> vec;
             std::stringstream ss;
-            for (auto i : node) {
+            for (auto i: node) {
                 ss.str("");
                 ss << i;
-                vec.intsert(LexicalCast<std::string, To>()(ss.str()));
+                vec.insert(LexicalCast<std::string, To>()(ss.str()));
             }
             return vec;
         }
@@ -103,10 +112,10 @@ namespace liucxi {
     template<typename From>
     class LexicalCast<std::set<From>, std::string> {
     public:
-        std::string operator() (const std::set<From> &v) {
+        std::string operator()(const std::set<From> &v) {
             YAML::Node node;
             std::stringstream ss;
-            for (auto &i : v) {
+            for (auto &i: v) {
                 node.push_back(YAML::Load(LexicalCast<From, std::string>()(i)));
             }
             ss << node;
@@ -117,15 +126,15 @@ namespace liucxi {
     template<typename To>
     class LexicalCast<std::string, std::map<std::string, To>> {
     public:
-        std::map<std::string, To> operator() (const std::string &v) {
+        std::map<std::string, To> operator()(const std::string &v) {
             YAML::Node node = YAML::Load(v);
             typename std::map<std::string, To> vec;
             std::stringstream ss;
             for (auto it = node.begin(); it != node.end(); ++it) {
                 ss.str("");
                 ss << it->second;
-                vec.intsert(std::make_pair(it->first.Scalar(),
-                                           LexicalCast<std::string, To>()(ss.str())));
+                vec.insert(std::make_pair(it->first.Scalar(),
+                                          LexicalCast<std::string, To>()(ss.str())));
             }
             return vec;
         }
@@ -134,10 +143,10 @@ namespace liucxi {
     template<typename From>
     class LexicalCast<std::map<std::string, From>, std::string> {
     public:
-        std::string operator() (const std::map<std::string, From> &v) {
+        std::string operator()(const std::map<std::string, From> &v) {
             YAML::Node node;
             std::stringstream ss;
-            for (auto &i : v) {
+            for (auto &i: v) {
                 node[i.first] = YAML::Load(LexicalCast<From, std::string>()(i.second));
             }
             ss << node;
@@ -149,11 +158,11 @@ namespace liucxi {
     /**
      * @brief 配置基类的实现类
      * */
-    template<typename T, typename FromStr = LexicalCast<std::string, T>
-                        , typename ToStr = LexicalCast<T, std::string>>
+    template<typename T, typename FromStr = LexicalCast<std::string, T>, typename ToStr = LexicalCast<T, std::string>>
     class ConfigVar : public ConfigVarBase {
     public:
         typedef std::shared_ptr<ConfigVar<T>> ptr;
+        typedef std::function<void( const T &old_value, const T &new_value)>  on_change;
 
         ConfigVar(const std::string &name, const T &default_val, const std::string &describe)
                 : ConfigVarBase(name, describe), m_val(default_val) {
@@ -166,8 +175,8 @@ namespace liucxi {
             try {
                 return ToStr()(getValue());
             } catch (std::exception &e) {
-                LUWU_LOG_ERROR(LUWU_LOG_ROOT()) << "ConfigVar::toString exception" << e.what()
-                                                << "convert: " << typeid(m_val).name() << "to string";
+                LUWU_LOG_ERROR(LUWU_LOG_ROOT()) << "ConfigVar::toString exception " << e.what()
+                                                << " convert: " << typeid(m_val).name() << "to string";
             }
             return "";
         }
@@ -179,18 +188,49 @@ namespace liucxi {
             try {
                 setValue(FromStr()(val));
             } catch (std::exception &e) {
-                LUWU_LOG_ERROR(LUWU_LOG_ROOT()) << "ConfigVar::fromString exception" << e.what()
-                                                << "convert: string to" << typeid(m_val).name();
+                LUWU_LOG_ERROR(LUWU_LOG_ROOT()) << "ConfigVar::fromString exception " << e.what()
+                                                << " convert: string to " << typeid(m_val).name();
             }
             return false;
         }
 
         T getValue() const { return m_val; }
 
-        void setValue(T val) { m_val = val; }
+        void setValue(const T &val) {
+            if (val == m_val) {
+                return;
+            }
+            for (auto &i : m_callbacks) {
+                i.second(m_val, val);
+            }
+            m_val = val;
+        }
+
+        std::string getTypeName() const override { return typeid(T).name(); }
+
+        void addListener(uint64_t key, on_change callback) {
+            m_callbacks[key] = callback;
+        }
+
+        void delListener(uint64_t key) {
+            m_callbacks.erase(key);
+        }
+
+        on_change getListener(uint64_t key) {
+            auto it = m_callbacks.find(key);
+            return it == m_callbacks.end() ? nullptr : it->second;
+        }
+
+        void clearListener() {
+            m_callbacks.clear();
+        }
 
     private:
         T m_val;
+        /**
+         * @brief 变更回调函数
+         * */
+        std::map<uint64_t, on_change> m_callbacks;
     };
 
     /**
@@ -206,12 +246,24 @@ namespace liucxi {
         template<typename T>
         static typename ConfigVar<T>::ptr lookup(const std::string &name, const T &default_val,
                                                  const std::string &description = "") {
+            /// 这种方法会出错
+            /*
             auto tmp = lookup<T>(name);
             if (tmp) {
                 LUWU_LOG_INFO(LUWU_LOG_ROOT()) << "lookup name = " << name << "exists";
                 return tmp;
+            }*/
+            auto it = s_data.find(name);
+            if (it != s_data.end()) {
+                auto tmp = std::dynamic_pointer_cast<ConfigVar<T>>(it->second);
+                if (tmp) {
+                    return tmp;
+                } else {
+                    LUWU_LOG_ERROR(LUWU_LOG_ROOT()) << "lookup name = " << name << " exists but type not "
+                                                    << typeid(T).name() << ", real type = " << it->second->getTypeName()
+                                                    << " " << it->second->toString();
+                }
             }
-
             if (name.find_first_not_of("qwertyuiopasdfghjklzxcvbnm._0123456789") != std::string::npos) {
                 LUWU_LOG_ERROR(LUWU_LOG_ROOT()) << "lookup name invalid" << name;
                 throw std::invalid_argument(name);
@@ -234,7 +286,7 @@ namespace liucxi {
             return std::dynamic_pointer_cast<ConfigVar<T>>(it->second);
         }
 
-        static void loadFromYaml(const YAML::Node& root);
+        static void loadFromYaml(const YAML::Node &root);
 
         static ConfigVarBase::ptr lookupBase(const std::string &name);
 
--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -9,11 +9,31 @@
 using namespace liucxi;
 
 ConfigVar<int>::ptr g_int = Config::lookup("system.port", (int)8080, "sys port");
-ConfigVar<float>::ptr g_float = Config::lookup("system.value", (float)12.3f, "sys value");
+
+ConfigVar<float>::ptr g_float = Config::lookup("system.port", (float)12.3f, "sys port");
+ConfigVar<std::vector<int>>::ptr g_vector = Config::lookup("system.vector",
+                                                           std::vector<int>{1,2},"system vector");
+ConfigVar<std::set<int>>::ptr g_set = Config::lookup("system.set",
+                                                           std::set<int>{1,2},"system set");
+ConfigVar<std::map<std::string, int>>::ptr g_map = Config::lookup("system.map",
+                                                           std::map<std::string , int>{{"k", 2}},"system map");
 
 void test_config() {
     std::cout << "g_int value: " << g_int->getValue() << std::endl;
-    std::cout << "g_float value: " << Config::lookup<float>("system.value")->toString() << std::endl;
+    std::cout << "g_float value: " << g_float->toString() << std::endl;
+    auto v = g_vector->getValue();
+    for (auto i : v) {
+        std::cout << "g_vector value: " << i << std::endl;
+    }
+    auto z = g_set->getValue();
+    for (auto i : z) {
+        std::cout << "g_set value: " << i << std::endl;
+    }
+    auto x = g_map->getValue();
+    for (const auto& i : x) {
+        std::cout << "g_map value: " << i.first << " " << i.second << std::endl;
+    }
+
 
     YAML::Node root = YAML::LoadFile("/home/liucxi/Documents/luwu/tests/log.yml");
 //    std::cout << root << std::endl;
@@ -21,12 +41,91 @@ void test_config() {
 
     std::cout << "g_int value: " << g_int->getValue() << std::endl;
     std::cout << "g_float value: " << g_float->toString() << std::endl;
+    v = g_vector->getValue();
+    for (auto i : v) {
+        std::cout << "g_vector value: " << i << std::endl;
+    }
+    z = g_set->getValue();
+    for (auto i : z) {
+        std::cout << "g_set value: " << i << std::endl;
+    }
+    x = g_map->getValue();
+    for (const auto& i : x) {
+        std::cout << "g_map value: " << i.first << " " << i.second << std::endl;
+    }
+}
 
+class Person {
+public:
+    std::string m_name;
+    int m_age = 0;
+    bool m_sex = false;
+    std::string toString() const {
+        std::stringstream ss;
+        ss << "[Person name=" << m_name
+           << " age=" << m_age
+           << " sex=" << m_sex
+           << "]";
+        return ss.str();
+    }
+
+    bool operator==(const Person &oth) const {
+        return m_name == oth.m_name;
+    }
+};
+
+namespace liucxi {
+    template<>
+    class LexicalCast<std::string, Person> {
+    public:
+        Person operator() (const std::string &v) {
+            YAML::Node node = YAML::Load(v);
+            Person p;
+            p.m_name = node["name"].as<std::string>();
+            p.m_age = node["age"].as<int>();
+            p.m_sex = node["sex"].as<int>();
+            return p;
+        }
+    };
+
+    template<>
+    class LexicalCast<Person, std::string> {
+    public:
+        std::string operator() (const Person &p) {
+            YAML::Node node;
+            node["name"] = p.m_name;
+            node["age"] = p.m_age;
+            node["sex"] = p.m_sex;
+            std::stringstream ss;
+            ss << node;
+            return ss.str();
+        }
+    };
+}
+
+ConfigVar<Person>::ptr g_person = Config::lookup("class.person", Person(), "class person");
+
+ConfigVar<std::vector<Person>>::ptr g_vec_person = Config::lookup("class.vec_person", std::vector<Person>(), "");
+
+void test_class() {
+
+    g_person->addListener(10, [](const Person &old_val, const Person &new_val){
+        std::cout << old_val.toString() << new_val.toString() << std::endl;
+    });
+    std::cout << "g_person value: " << g_person->toString() << std::endl;
+    std::cout << "g_vec_person value: " << g_vec_person->toString() << std::endl;
+
+    YAML::Node root = YAML::LoadFile("/home/liucxi/Documents/luwu/tests/log.yml");
+    Config::loadFromYaml(root);
+
+    std::cout << "g_person value: " << g_person->toString() << std::endl;
+    std::cout << "g_vec_person value: " << g_vec_person->toString() << std::endl;
 
 }
 int main(int argc, char *argv[]) {
 
-    test_config();
+//     test_config();
+    test_class();
     return 0;
 }
 
