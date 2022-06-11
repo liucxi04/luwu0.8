e3d167842b4f60773a393ac487c533c1e2810e04
liucxi <liucxi@foxmail.com>
2022-06-12 00:26:57 +0800

    http_connection code finish

--- tests/test_http_connection.cpp
+++ tests/test_http_connection.cpp
@@ -11,62 +11,62 @@
 
 static liucxi::Logger::ptr g_logger = LUWU_LOG_ROOT();
 
-//void test_pool() {
-//    liucxi::http::HttpConnectionPool::ptr pool(new liucxi::http::HttpConnectionPool(
-//            "www.midlane.top", "", 80, 10, 1000 * 30, 5));
-//
-//    liucxi::IOManager::GetThis()->addTimer(
-//            1000, [pool]() {
-//                auto r = pool->doGet("/", 300);
-//                std::cout << r->toString() << std::endl;
-//            },
-//            true);
-//}
+void test_pool() {
+    liucxi::http::HttpConnectionPool::ptr pool(new liucxi::http::HttpConnectionPool(
+            "www.sylar.top", "", 80, 1000 * 30, 5));
+
+    liucxi::IOManager::GetThis()->addTimer(
+            1000, [pool]() {
+                auto r = pool->doGet("/", 300);
+                std::cout << r->toString() << std::endl;
+            },
+            true);
+}
 
 void run() {
-    liucxi::Address::ptr addr = liucxi::Address::LookupAnyIPAddress("www.liucxi.xyz:80");
-    if (!addr) {
-        LUWU_LOG_INFO(g_logger) << "get addr error";
-        return;
-    }
-
-    liucxi::Socket::ptr sock = liucxi::Socket::CreateTCP(addr);
-    bool rt = sock->connect(addr);
-    if (!rt) {
-        LUWU_LOG_INFO(g_logger) << "connect " << *addr << " failed";
-        return;
-    }
-
-    liucxi::http::HttpConnection::ptr conn(new liucxi::http::HttpConnection(sock));
-    liucxi::http::HttpRequest::ptr req(new liucxi::http::HttpRequest);
-
-    req->setHeader("host", "www.liucxi.xyz");
+//    liucxi::Address::ptr addr = liucxi::Address::LookupAnyIPAddress("www.liucxi.xyz:80");
+//    if (!addr) {
+//        LUWU_LOG_INFO(g_logger) << "get addr error";
+//        return;
+//    }
+//
+//    liucxi::Socket::ptr sock = liucxi::Socket::CreateTCP(addr);
+//    bool rt = sock->connect(addr);
+//    if (!rt) {
+//        LUWU_LOG_INFO(g_logger) << "connect " << *addr << " failed";
+//        return;
+//    }
+//
+//    liucxi::http::HttpConnection::ptr conn(new liucxi::http::HttpConnection(sock));
+//    liucxi::http::HttpRequest::ptr req(new liucxi::http::HttpRequest);
+//
+//    req->setHeader("host", "www.liucxi.xyz");
 //    // 小bug，如果设置了keep-alive，那么要在使用前先调用一次init
 //    req->setHeader("connection", "keep-alive");
 //    req->init();
-    std::cout << "req:" << std::endl
-              << *req << std::endl;
+//    std::cout << "req:" << std::endl
+//              << *req << std::endl;
+//
+//    conn->sendRequest(req);
+//    auto rsp = conn->recvResponse();
+//
+//    if (!rsp) {
+//        LUWU_LOG_INFO(g_logger) << "recv response error";
+//        return;
+//    }
+//    std::cout << "rsp:" << std::endl
+//              << *rsp << std::endl;
 
-    conn->sendRequest(req);
-    auto rsp = conn->recvResponse();
-
-    if (!rsp) {
-        LUWU_LOG_INFO(g_logger) << "recv response error";
-        return;
-    }
-    std::cout << "rsp:" << std::endl
-              << *rsp << std::endl;
-
-    std::cout << "=========================" << std::endl;
-
-//    auto r = liucxi::http::HttpConnection::DoGet("http://www.midlane.top/wiki/", 300);
-//    std::cout << "result=" << r->result
-//              << " error=" << r->error
+//    std::cout << "=========================" << std::endl;
+//
+//    auto r = liucxi::http::HttpConnection::DoGet("http://www.liucxi.xyz/", 300);
+//    std::cout << "error=" << (int)r->error
+//              << " errstr=" << r->errstr
 //              << " rsp=" << (r->response ? r->response->toString() : "")
 //              << std::endl;
 //
 //    std::cout << "=========================" << std::endl;
-//    test_pool();
+    test_pool();
 }
 
 int main(int argc, char **argv) {
--- tests/test_uri.cpp
+++ tests/test_uri.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 int main(int argc, char * argv[]) {
-    auto uri = liucxi::URI::Create("http://a:b@host.com:8080/p/a/t/h?query=string#hash");
+    auto uri = liucxi::URI::Create("http://a:b@host.com/p/a/t/h?query=string#hash");
     std::cout << uri->toString() << std::endl;
     auto addr = uri->createAddress();
     std::cout << *addr << std::endl;
